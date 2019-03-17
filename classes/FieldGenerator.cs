d7a15ab2c41317cb06e0042a908d8ce083c4e748
denis <erami.ukr@gmail.com>
2019-03-17 17:41:59 +0200

    Basic generator fixed

--- classes/FieldGenerator.cs
+++ classes/FieldGenerator.cs
@@ -3,7 +3,7 @@ using System;
 namespace Mined_Out {
     public static class FieldGenerator {
         public static Field Generate() {
-            Field field = PreGenerateField(7, 7);
+            Field field = PreGenerateField(10, 9);
 
             return field;
         }
@@ -26,7 +26,7 @@ namespace Mined_Out {
             // Left and right edge
             for(int i = 0; i < height - 1; i++) {
                 field[i, 0] = new Wall();
-                field[i, height - 1] = new Wall();
+                field[i, width - 1] = new Wall();
             }
 
             return field;
